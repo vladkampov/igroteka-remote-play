2b5cbbe9c0759cdd4efd3f78272c8c07daeb7eae
vladkampov <vlad@geeny.io>
2018-06-25 18:19:56 +0300

    Implement fancy PacMan loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { Grid } from 'react-bootstrap';
+import './Loader.scss';
 
 export default () => (
-  <div>Loading</div>
+  <div className="Loader">
+    <Grid>
+      <div className="pacman" />
+      <div className="dot" />
+    </Grid>
+  </div>
 );
